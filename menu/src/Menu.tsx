2958c9f18b95a9e83675d50d01769f9c2595657b
Ogbu Ezekiel Ike <zico@zico.local>
2022-02-28 03:51:24 +0100

    deploy

--- menu/src/Menu.tsx
+++ menu/src/Menu.tsx
@@ -13,17 +13,17 @@ const Menu = ({ menuItems }: Props) => {
       {menuItems.map((item) => {
         const { price, title, img, desc, id } = item;
         return (
-            <article key={id} className='menu-item'>
-              <img src={img} alt={title} className='photo' />
-              <div className='item-info'>
-                <header>
-                  <h4>{title}</h4>
-                  <h4 className='price'>${price}</h4>
-                </header>
-                <p className='item-text'>{desc}</p>
-              </div>
-            </article>
-          );
+          <article key={id} className="menu-item">
+            <img src={img} alt={title} className="photo" />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="price">${price}</h4>
+              </header>
+              <p className="item-text">{desc}</p>
+            </div>
+          </article>
+        );
       })}
     </div>
   );
